9c0e6abe7035c3aa34c564739b03bc4b7fe28df4
Matthias Wang <matthiaswang60@gmail.com>
2023-06-01 14:51:56 +0800

    nah new lib didnt work

--- app/src/main/java/com/ivp/cuecaphelper/MainActivity.java
+++ app/src/main/java/com/ivp/cuecaphelper/MainActivity.java
@@ -1,68 +1,46 @@
 package com.ivp.cuecaphelper;
 
-import android.Manifest;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
-import android.os.Bundle;
-import android.os.Environment;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.speech.tts.TextToSpeech;
+import android.view.SurfaceHolder;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.herohan.uvcapp.CameraHelper;
+import com.herohan.uvcapp.ICameraHelper;
 import com.hjq.permissions.XXPermissions;
-import com.jiangdg.ausbc.MultiCameraClient;
-import com.jiangdg.ausbc.base.CameraActivity;
-import com.jiangdg.ausbc.callback.ICaptureCallBack;
-import com.jiangdg.ausbc.camera.CameraUVC;
-import com.jiangdg.ausbc.camera.bean.CameraRequest;
-import com.jiangdg.ausbc.camera.bean.PreviewSize;
-import com.jiangdg.ausbc.render.env.RotateType;
-import com.jiangdg.ausbc.widget.AspectRatioSurfaceView;
-import com.jiangdg.ausbc.widget.IAspectRatio;
+import com.serenegiant.usb.Size;
+import com.serenegiant.usb.UVCCamera;
+import com.serenegiant.widget.AspectRatioSurfaceView;
 
-
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
-import java.util.logging.Logger;
 
-public class MainActivity extends CameraActivity implements View.OnClickListener {
-
-    //Stores MainActivity view
-    public View rootView;
-    //Stores text to speech service
+public class MainActivity extends AppCompatActivity {
+    //Global TTS service
     public TextToSpeech tts;
 
-    @Nullable
+    //Camera Helpers
+    private ICameraHelper mCameraHelper;
+    private AspectRatioSurfaceView mCameraViewMain;
+    private ICameraHelper.StateCallback mStateListener;
+
     @Override
-    protected View getRootView(@NonNull LayoutInflater layoutInflater) {
-        rootView = getLayoutInflater().inflate(R.layout.activity_main, null);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         //Request perms
         List<String> needPermissions = new ArrayList<>();
-        needPermissions.add(Manifest.permission.CAMERA);
-        needPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        needPermissions.add(android.Manifest.permission.CAMERA);
 
         XXPermissions.with(this)
                 .permission(needPermissions)
@@ -80,109 +58,85 @@ public class MainActivity extends CameraActivity implements View.OnClickListener
             }
         });
 
-        //Bind USB events
-        IntentFilter filter = new IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-        registerReceiver(mUsbAttachReceiver , filter);
-        filter = new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED);
-        registerReceiver(mUsbDetachReceiver , filter);
-
-
-
-        Button button = rootView.findViewById(R.id.takePhotoBtn);
-        button.setOnClickListener(this);
-
-        return rootView;
+        //Init Camera stuff
+        mStateListener = new StateListenerClass();
+        mCameraViewMain = (AspectRatioSurfaceView) findViewById(R.id.surfaceView);
+        mCameraViewMain.getHolder().addCallback(new SurfaceHolderCallback());
+        mCameraHelper = new CameraHelper();
+        mCameraHelper.setStateCallback(mStateListener);
     }
 
-    //region UVC Camera Event Handlers
-    @NonNull
-    @Override
-    public CameraRequest getCameraRequest() {
-        return new CameraRequest.Builder()
-                .setPreviewWidth(640)
-                .setPreviewHeight(480)
-                .setRenderMode(CameraRequest.RenderMode.NORMAL)
-                .setDefaultRotateType(RotateType.ANGLE_0)
-                .setAudioSource(CameraRequest.AudioSource.NONE)
-                .setAspectRatioShow(true)
-                .setCaptureRawImage(true)
-                .setRawPreviewData(true)
-                .create();
-    }
 
-    @Nullable
-    @Override
-    protected IAspectRatio getCameraView() {
-        return (AspectRatioSurfaceView) rootView.findViewById(R.id.cameraView);
-    }
-
-    @Nullable
-    @Override
-    protected ViewGroup getCameraViewContainer() {
-        return rootView.findViewById(R.id.cameraViewContainer);
-    }
-
-    @Override
-    public void onCameraState(@NonNull MultiCameraClient.ICamera iCamera, @NonNull State state, @Nullable String s) {}
-
-    @Override
-    public UsbDevice getDefaultCamera() {
-        return (UsbDevice)getIntent().getParcelableExtra(UsbManager.EXTRA_DEVICE);
-    }
-
-    @Override
-    public void onClick(View view) {
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
-        Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
-        while(deviceIterator.hasNext()){
-            UsbDevice device = deviceIterator.next();
-            Log.i("IVPUSB", Integer.toString(device.getProductId()));
-            Log.i("IVPUSB", Integer.toString(device.getVendorId()));
+    private class StateListenerClass implements ICameraHelper.StateCallback {
+        @Override
+        public void onAttach(UsbDevice device) {
+            tts.speak("Webcam attached.", TextToSpeech.QUEUE_ADD, null, String.valueOf(java.util.UUID.randomUUID()));
+            mCameraHelper.selectDevice(device);
         }
-//        captureImage(new ICaptureCallBack() {
-//            @Override
-//            public void onBegin() {}
-//
-//            @Override
-//            public void onError(@Nullable String s) {
-//                Snackbar.make(rootView, s, Snackbar.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onComplete(@Nullable String s) {}
-//        }, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/image.jpg");
-    }
 
-    BroadcastReceiver mUsbAttachReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
+        @Override
+        public void onDeviceOpen(UsbDevice device, boolean isFirstOpen) {
+            mCameraHelper.openCamera(new Size(UVCCamera.FRAME_FORMAT_YUYV, 640, 480, 25, new ArrayList<>(25)));
+        }
 
-            if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
-                tts.speak("Webcam attached.", TextToSpeech.QUEUE_ADD, null, String.valueOf(java.util.UUID.randomUUID()));
+        @Override
+        public void onCameraOpen(UsbDevice device) {
+            mCameraHelper.startPreview();
+            mCameraViewMain.setAspectRatio(640, 480);
+            mCameraHelper.setPreviewSize(new Size(UVCCamera.FRAME_FORMAT_YUYV, 640, 480, 25, new ArrayList<>(25)));
+            mCameraHelper.addSurface(mCameraViewMain.getHolder().getSurface(), false);
+        }
+
+        @Override
+        public void onCameraClose(UsbDevice device) {
+            if (mCameraHelper != null) {
+                mCameraHelper.removeSurface(mCameraViewMain.getHolder().getSurface());
             }
         }
-    };
 
-    BroadcastReceiver mUsbDetachReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
+        @Override
+        public void onDeviceClose(UsbDevice device) {
 
-            if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-                UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                if (device != null) {
-                    tts.speak("Webcam detached.", TextToSpeech.QUEUE_ADD, null, String.valueOf(java.util.UUID.randomUUID()));
-                }
+        }
+
+        @Override
+        public void onDetach(UsbDevice device) {
+            tts.speak("Webcam detached.", TextToSpeech.QUEUE_ADD, null, String.valueOf(java.util.UUID.randomUUID()));
+        }
+
+        @Override
+        public void onCancel(UsbDevice device) {
+
+        }
+    }
+
+    ;
+
+    private class SurfaceHolderCallback implements SurfaceHolder.Callback {
+
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+            if (mCameraHelper != null) {
+                mCameraHelper.addSurface(surfaceHolder.getSurface(), false);
             }
         }
-    };
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+            if (mCameraHelper != null) {
+                mCameraHelper.removeSurface(surfaceHolder.getSurface());
+            }
+        }
+    }
+
     @Override
-    public void onDestroy() {
-        unregisterReceiver(mUsbDetachReceiver);
-        unregisterReceiver(mUsbAttachReceiver);
+    protected void onDestroy() {
         super.onDestroy();
+        mCameraHelper.release();
     }
-
-    //endregion
 
 }
