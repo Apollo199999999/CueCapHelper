b0face44006dca0aa3840a2e17f6319f53bce71d
Matthias Wang <matthiaswang60@gmail.com>
2023-06-14 22:43:42 +0800

    Face Detection with MLKit

--- app/src/main/java/com/ivp/cuecaphelper/MainActivity.java
+++ app/src/main/java/com/ivp/cuecaphelper/MainActivity.java
@@ -5,52 +5,43 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
-import android.os.Bundle;
-import android.os.Environment;
 import android.speech.tts.TextToSpeech;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
 import com.hjq.permissions.XXPermissions;
 import com.jiangdg.ausbc.MultiCameraClient;
 import com.jiangdg.ausbc.base.CameraActivity;
-import com.jiangdg.ausbc.callback.ICaptureCallBack;
-import com.jiangdg.ausbc.camera.CameraUVC;
 import com.jiangdg.ausbc.camera.bean.CameraRequest;
-import com.jiangdg.ausbc.camera.bean.PreviewSize;
 import com.jiangdg.ausbc.render.env.RotateType;
-import com.jiangdg.ausbc.widget.AspectRatioSurfaceView;
 import com.jiangdg.ausbc.widget.AspectRatioTextureView;
 import com.jiangdg.ausbc.widget.IAspectRatio;
 
-
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.Logger;
 
 public class MainActivity extends CameraActivity {
 
@@ -67,6 +58,9 @@ public class MainActivity extends CameraActivity {
 
     //Interval in milliseconds on how often to get frames
     private int frameInterval = 100;
+
+    //FaceDetector object from MLKit
+    FaceDetector faceDetector;
 
     //endregion
 
@@ -97,6 +91,15 @@ public class MainActivity extends CameraActivity {
             }
         });
 
+        //Init FaceDetector
+        FaceDetectorOptions faceDetectorOptions =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+                        .build();
+        faceDetector = FaceDetection.getClient(faceDetectorOptions);
+
         //Bind USB events
         IntentFilter filter = new IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED);
         registerReceiver(mUsbAttachReceiver, filter);
@@ -116,8 +119,8 @@ public class MainActivity extends CameraActivity {
     @Override
     public CameraRequest getCameraRequest() {
         return new CameraRequest.Builder()
-                .setPreviewWidth(640)
-                .setPreviewHeight(480)
+                .setPreviewWidth(490)
+                .setPreviewHeight(360)
                 .setRenderMode(CameraRequest.RenderMode.NORMAL)
                 .setDefaultRotateType(RotateType.ANGLE_0)
                 .setAudioSource(CameraRequest.AudioSource.NONE)
@@ -179,9 +182,10 @@ public class MainActivity extends CameraActivity {
         super.onDestroy();
     }
 
-    //endregion
+    //endregion%%%
 
-    //region OpenCV shit
+    //region Face detection/Tensorflow shit (god save me I'm gonna kill Caden after this)
+
     private class GetFrames extends TimerTask {
         //This thing's a goddamn mess
         @Override
@@ -190,18 +194,58 @@ public class MainActivity extends CameraActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        //Get bitmaps from the webcam and display it in the imageView
+                        //Get bitmaps from the webcam display
                         AspectRatioTextureView aspectRatioTextureView =
                                 (AspectRatioTextureView) rootView.findViewById(R.id.cameraView);
+                        ImageView framesImageView = (ImageView) rootView.findViewById(R.id.framesView);
+
                         Bitmap frame = aspectRatioTextureView.getBitmap();
 
-                        ImageView framesImageView = (ImageView) rootView.findViewById(R.id.framesView);
-                        framesImageView.setImageBitmap(frame);
+                        //Put bitmap in face detector
+                        InputImage image = InputImage.fromBitmap(frame, 0);
+
+                        Task<List<Face>> result =
+                                faceDetector.process(image)
+                                        .addOnSuccessListener(
+                                                new OnSuccessListener<List<Face>>() {
+                                                    @Override
+                                                    public void onSuccess(List<Face> faces) {
+                                                        if (faces.size() > 0) {
+                                                            for (Face face : faces) {
+                                                                Rect bounds = face.getBoundingBox();
+
+                                                                //Draw the bounding rectangle
+                                                                Bitmap frameMutable = frame.copy(Bitmap.Config.ARGB_8888, true);
+                                                                Canvas canvas = new Canvas(frameMutable);
+                                                                Paint myPaint = new Paint();
+                                                                myPaint.setColor(Color.rgb(0, 0, 0));
+                                                                myPaint.setStrokeWidth(10);
+                                                                myPaint.setStyle(Paint.Style.STROKE);
+                                                                canvas.drawRect(bounds.left, bounds.top, bounds.right, bounds.bottom, myPaint);
+
+                                                                framesImageView.setImageBitmap(frameMutable);
+                                                            }
+                                                        }
+                                                        else {
+                                                            //Just set the unmodified frame bitmap
+                                                            framesImageView.setImageBitmap(frame);
+                                                        }
+                                                    }
+                                                })
+                                        .addOnFailureListener(
+                                                new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        //Just set the unmodified frame bitmap
+                                                        framesImageView.setImageBitmap(frame);
+                                                    }
+                                                });
                     }
                 });
 
             }
         }
     }
+
     //endregion
 }
